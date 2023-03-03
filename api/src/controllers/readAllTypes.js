7ac68a6eedb74c83886417de4e23210fd02e998f
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-03 00:34:50 -0600

    controlador ReadAllTypes

--- api/src/controllers/readAllTypes.js
+++ api/src/controllers/readAllTypes.js
@@ -1,6 +1,12 @@
 
-module.exports = ()=>{
+const { Tipo } = require("../database")
 
+module.exports = async () => {
+    const allTipos = await Tipo.findAll()
+    const resultado = allTipos.map(tipo => {
+        return tipo.dataValues
+    })    
+    return resultado
 }
 
 
