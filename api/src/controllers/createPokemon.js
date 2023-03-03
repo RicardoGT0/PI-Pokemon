8f7fa86b6cb034b730301e14faca95555f7c67e1
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-03 01:35:29 -0600

    Incremento manual en el ID Pokemon

--- api/src/controllers/createPokemon.js
+++ api/src/controllers/createPokemon.js
@@ -3,7 +3,11 @@ const { Pokemon } = require("../database")
 module.exports = async (pokemon) => {
     const pokCreado = await Pokemon.create(pokemon)
     pokCreado.addTipos(pokemon.Tipo)
-    console.log(pokCreado.dataValues)
-    return pokCreado.dataValues
+    resultado = {
+        ...pokCreado.dataValues,
+        ID: pokCreado.dataValues.ID + 1008
+    }
+    console.log(resultado)
+    return resultado
 }
 
