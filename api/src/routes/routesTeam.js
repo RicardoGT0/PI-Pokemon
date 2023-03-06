1fc7bc8a4781ee2968d945468f84f0b63e78df5c
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-06 00:22:28 -0600

    Create Routes of Team

--- api/src/routes/routesTeam.js
+++ api/src/routes/routesTeam.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
+const addOfTeam = require("../controllers/team/addOfTeam");
+const deleteOfTeam = require("../controllers/team/deleteOfTeam");
+const readTeam = require("../controllers/team/readTeam");
 
 const routes = new Router();
 
 routes.get('/:id', (req, res) => {
     const { id } = req.params
     try {
-        const respuesta = readFavs(id)
+        const respuesta = readTeam(id)
         res.status(200).json(respuesta)
     } catch (error) {
         res.status(400).json({ Error: error.message })
@@ -16,7 +19,7 @@ routes.post('/:id', (req, res) => {
     const { id } = req.params //user ID
     const { poke } = req.query //pokemon ID
     try {
-        const respuesta = createFavs(id, poke)
+        const respuesta = addOfTeam(id, poke)
         res.status(200).json(respuesta)
     } catch (error) {
         res.status(400).json({ Error: error.message })
@@ -27,7 +30,7 @@ routes.delete('/:id', (req, res) => {
     const { id } = req.params //user ID
     const { poke } = req.query //pokemon ID
     try {
-        const respuesta = deleteFavs(id, poke)
+        const respuesta = deleteOfTeam(id, poke)
         res.status(200).json(respuesta)
     } catch (error) {
         res.status(400).json({ Error: error.message })
