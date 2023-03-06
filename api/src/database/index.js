5c63efd25b96ce62c299003b624214e5bfe6c82f
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-05 19:06:45 -0600

    Implementacion de PokeNames en cargaDB Index

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -5,6 +5,9 @@ const Tipo = require('./models/Tipo');
 const Relationships = require('./models/Relationships');
 const User = require('./models/User');
 const llenarTipos = require('./Utils/downloadTypes');
+const { getAllPokeNames } = require('../controllers/utils/pokeNames');
+
+
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const database = new Sequelize(
@@ -12,7 +15,7 @@ const database = new Sequelize(
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-      force: true,
+      force: false,
    }
 );
 
@@ -20,12 +23,15 @@ Pokemon(database)
 Tipo(database)
 User(database)
 Relationships(database)
-llenarTipos(database)
+/* llenarTipos(database)
    .then(message => console.log(message))
-   .catch(error => console.log(error))
+   .catch(error => console.log(error)) */
+   
+getAllPokeNames(database.models.Pokemon);
 console.log('DataBase Model = ', database.models);
 
 module.exports = {
    ...database.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
    database, // para importart la conexión { conn } = require('./db.js');
 };
+
