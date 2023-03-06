e2735bf361645993a9fc11baa6ef575200a2d9b3
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-06 00:21:42 -0600

    Correccion GetAllPokemons

--- api/src/controllers/pokemon/readAllPokemon.js
+++ api/src/controllers/pokemon/readAllPokemon.js
@@ -4,7 +4,7 @@ const { pokeNames } = require("../utils/pokeNames")
 module.exports = async (page = 1) => {
     const listaPokes = []
     for (let id = 1; id <= 12; id++) {
-        if (id + ((page - 1) * 12) < pokeNames.length) {
+        if (id + ((page - 1) * 12) <= pokeNames.length) {
             const pok = await readPokemonByID(id + ((page - 1) * 12))
             listaPokes.push(pok)
         }
