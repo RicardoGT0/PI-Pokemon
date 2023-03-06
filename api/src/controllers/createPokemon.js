35be9c2c7db6395e92e43aa77906452e24866127
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-05 19:04:42 -0600

    implementacion de pokenames

--- api/src/controllers/createPokemon.js
+++ api/src/controllers/createPokemon.js
@@ -1,11 +1,14 @@
 const { Pokemon } = require("../database")
 const readPokemonByID = require("./readPokemonByID")
+const { pokeNames } = require("./utils/pokeNames")
 
-module.exports = async (pokemon) => {
+module.exports = async (pokemon) => {    
+    pokemon.Nombre=pokemon.Nombre.toLowerCase()
     const pokCreado = await Pokemon.create(pokemon)
     await pokCreado.addTipos(pokemon.Tipo)
     const id = pokCreado.dataValues.ID + 1008
     const pok = await readPokemonByID(id)
+    pokeNames.push(pok.Nombre)
     return pok
 }
 
