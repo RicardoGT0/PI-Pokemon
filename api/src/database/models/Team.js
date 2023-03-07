68edccf5eaabd7a1371e4d0aeeadfc5fd5525e83
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-06 19:30:07 -0600

    Actualizacion del Modelo Team

--- api/src/database/models/Team.js
+++ api/src/database/models/Team.js
@@ -3,6 +3,11 @@ const { DataTypes } = require('sequelize');
 module.exports = (database) => {
 
     database.define('Team', {
+        memberID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+
+        },
         UserID: {
             type: DataTypes.INTEGER,
             allowNull: false,
