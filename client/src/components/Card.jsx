bc0281c952aeec419faf84f36cfb31736b831309
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-13 17:18:22 -0600

    Correccion nombres de clases

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,17 +5,17 @@ import './Card.css'
 export default function Card({ ID, Nombre, Imagen, Tipo = [] }) {
     const [hover, setHover] = useState(false)
     const showDescription = () => {
-        return <div className='divDescription'>
-            <div className='divID'># {ID}</div>
-            <div className='divName'>{Nombre}</div>
-            <div className='divTypes'>{Tipo.join(' / ')}</div>
+        return <div className='divCardDescription'>
+            <div className='divCardID'># {ID}</div>
+            <div className='divCardName'>{Nombre}</div>
+            <div className='divCardTypes'>{Tipo.join(' / ')}</div>
         </div>
     }
 
     return (
         <Link to={ID==='0'?'/create':`/details/${ID}`} >
             <div className={`divCard`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} >
-                <div className={`divimage ${hover ? 'blur' : null}`} >
+                <div className={`divCardImage ${hover ? 'blur' : null}`} >
                     <img className='pokeImage' src={Imagen} alt={Nombre} />
                 </div>
                 {hover ? showDescription() : null}
