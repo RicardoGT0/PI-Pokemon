ba49f50a8dcf508780df2efcedcd6beab82bf14a
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-13 21:37:53 -0600

    About Completo

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import "./About.css";
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className='divHeaderAbout'>
+        <h1 className='headerAboutText'>P I  -  P o k é m o n </h1>
+        <h1 className='headerAboutText'>Fecha de elaboracion de la pagina: Marzo 2023</h1>
+      </div>
+      <div className='divAbout'>
+        <h1 className='textabout'>Acerca del desarrollador</h1>
+        <h1 className='textabout'>Nombre: Ricardo González Tello</h1>
+        <h1 className='textabout'>Titulo: M. en T. de Computo</h1>
+        <div>
+          <a className='linkTextabout'
+            target="_blank"
+            rel="noopener noreferrer"
+            href='https://www.linkedin.com/in/ricardo-gonzález-tello-7643bb1a9'
+          >LinkedIn</a>
+        </div>
+        <div>
+          <a className='linkTextabout'
+            target='_blank'
+            rel="noopener noreferrer"
+            href="https://github.com/RicardoGT0"
+          >GitHub</a>
+        </div>
+      </div>
+    </>
   )
 }
