f77e12123c6e8015938ab74bd6d71efec108f176
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-14 22:51:50 -0600

    Inicio Creacion Team

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './Team.css'
 
 export default function Team() {
   return (
-    <div>Team</div>
+    <>
+      <div className='divHeaderTeam'>
+        <h1 className='headerTeamText'>Tus Compañeros de Aventuras son: </h1>
+      </div>
+      <div className='divTeam'>Team</div>
+    </>
   )
 }
