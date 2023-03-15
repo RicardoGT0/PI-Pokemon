145c2b06123a3ad0b2ba38408270359772a9c97d
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-03-14 22:49:11 -0600

    Correccion Sort

--- client/src/components/SortModel.jsx
+++ client/src/components/SortModel.jsx
@@ -1,57 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setSort } from '../redux/actions';
-import CheckB from './CheckB';
 import './FilterModel.css'
 
-export default function SortModel({ name, text, listOfFilters }) {
-    const [show, setShow] = useState(false)
+export default function SortModel({ name, text, listOfSorters }) {
     const { sort } = useSelector((state) => state);
-    const [checked, setChecked] = useState([]);
+    const [option, setOption] = useState(text)
     const dispatch = useDispatch()
 
     useEffect(() => {
         if (sort[name])
-            setChecked(sort[name])
-    }, [])
+            setOption(sort[name])
+    }, [sort])
 
-    const handleCheck = (event) => {
-        var updatedList = [...checked];
+    const lista = listOfSorters.map((item) => item.Nombre)
 
-        if (event.target.checked) {
-            if (updatedList.length < 2)
-                updatedList = [...checked, event.target.value];
-            else {
-                event.target.checked = false
-                alert('Te recuerdo que los Pokemon solo pueden tener 2 tipos como maximo')
-            }
-        } else {
-            updatedList.splice(checked.indexOf(event.target.value), 1);
-        }
-        setChecked(updatedList);
-        dispatch(setSort({ [name]: updatedList }))
+    const changeOption = () => {
+        let index = lista.indexOf(option)
+        if (index === lista.length - 1) index = -1
+        setOption(lista[index + 1])
+        dispatch(setSort({ [name]: lista[index+1] }))
     }
-
-
-
-    const lista = listOfFilters.map((item) => {
-        return (
-            <CheckB
-                key={item.ID}
-                id={item.ID}
-                name={item.Nombre}
-                handleCheck={handleCheck}
-                checked={checked}
-            />
-        )
-    })
 
     return (
         <div className='divFilterModel'>
-            <span className='spanTextFilter' onClick={() => setShow(!show)}>
-                {name}: {checked.length ? checked.join(', ') : text}
+            <span className='spanTextFilter' onClick={changeOption}>
+                {name}: {option}
             </span>
-            {show ? lista : null}
         </div>
     )
 }
--- client/src/components/Sorters.jsx
+++ client/src/components/Sorters.jsx
@@ -3,12 +3,12 @@ import './Filters.css'
 import SortModel from './SortModel'
 
 export default function Sorters() {
-    const listSortName=[{ ID: 0, Nombre: 'Sin Orden Alfabetico' }, { ID: 1, Nombre: 'A-Z' }, { ID: 2, Nombre: 'Z-A' } ]
-    const listSortAtk=[{ ID: 0, Nombre: 'Sin Orden por Ataque' }, { ID: 1, Nombre: 'Ataque Ascendente' }, { ID: 2, Nombre: 'Ataque Descendente' } ]
+  const listSortName = [{ ID: 0, Nombre: 'Sin Orden Alfabetico' }, { ID: 1, Nombre: 'A-Z' }, { ID: 2, Nombre: 'Z-A' }]
+  const listSortAtk = [{ ID: 0, Nombre: 'Sin Orden por Ataque' }, { ID: 1, Nombre: 'Ataque Ascendente' }, { ID: 2, Nombre: 'Ataque Descendente' }]
   return (
     <div className='divFilters'>
-        <SortModel name={'Orden Alfabetico'} listOfFilters={listSortName} text={'Sin Orden Alfabetico'} />
-      <SortModel name={'Orden por Ataque'} listOfFilters={listSortAtk} text={'Sin Orden por Ataque'} />
+      <SortModel name={'Orden Alfabetico'} listOfSorters={listSortName} text={'Sin Orden Alfabetico'} />
+      <SortModel name={'Orden por Ataque'} listOfSorters={listSortAtk} text={'Sin Orden por Ataque'} />
     </div>
   )
 }
